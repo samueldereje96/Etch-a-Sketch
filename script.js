0a5e42ceaf63d78c643d4ea850792d52049cc1df
samueldereje96 <samueldereje1996@gmail.com>
2024-08-06 16:17:02 +0300

    add improved html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const drawBoard = document.querySelector(".draw-board");
-    const eraserButton = document.getElementById("eraser");
     const randomButton = document.getElementById("randomize");
+    const eraserButton = document.getElementById("eraser");
     const gridSize = document.getElementById("grid-size");
-    const gridSizeLabel = document.getElementById("grid-size-label")
+    const gridSizeLabel = document.getElementById("grid-size-label");
 
     defaultPenColor = "#000000";
     defaultBgColor = "#ffffff";
@@ -11,62 +11,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let pen = defaultPenColor;
     let bgColor = defaultBgColor;
-    let isEraserActive = false;
     let isRandomActive = false;
+    let isEraserActive = false;
 
     createGrid(defaultGridSize);
 
     function createGrid (size) {
         drawBoard.innerHTML = "";
-        const squareSize = 640 / size; 
+        const sqaureSize = 580 / size;
+
         for (let i = 0; i < size; i++) {
             for (let j = 0; j < size; j++) {
                 const square = document.createElement("div");
                 square.classList.add("grid-square");
-                square.style.width = `${squareSize}px`;
-                square.style.height = `${squareSize}px`;
+                square.style.width = `${sqaureSize}px`;
+                square.style.height = `${sqaureSize}px`;
                 drawBoard.appendChild(square);
             }
         }
-        draw();
+        draw()
     }
-    
     function draw() {
         const squares = document.querySelectorAll(".grid-square");
-        squares.forEach(square =>
+        squares.forEach(square => {
             square.addEventListener("click", () => {
-                if (isRandomActive) {
-                    square.style.backgroundColor = getRandomColor();
-                } else if (isEraserActive) {
+                if (isEraserActive) {
                     square.style.backgroundColor = bgColor;
+                } else if (isRandomActive) {
+                    square.style.backgroundColor = getRandomColor();
                 } else {
                     square.style.backgroundColor = pen;
                 }
             })
-        )
-    } 
-    function getRandomColor () {
-        const letters = "0123456789ABCDEF";
-        let color = "#";
-
+        })
+    }
+    function getRandomColor() {
+        letters = "0123456789ABCDEF";
+        color = "#";
         for (let i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
-
     document.getElementById("colorInput").addEventListener("input", (event) => {
         pen = event.target.value;
         isRandomActive = false;
+        isEraserActive = false;
     })
     document.getElementById("bgInput").addEventListener("input", (event) => {
         bgColor = event.target.value;
         const squares = document.querySelectorAll(".grid-square");
         squares.forEach(square => {
             square.style.backgroundColor = bgColor;
-        }
-    );
-    });
+        })
+    })
     document.getElementById("randomize").addEventListener("click", () => {
         isRandomActive = !isRandomActive;
         isEraserActive = false;
@@ -80,24 +78,20 @@ document.addEventListener("DOMContentLoaded", () => {
         randomButton.classList.remove("active");
     })
     gridSize.addEventListener("input", (event) => {
-        const size = event.target.value;
-        gridSizeLabel.textContent = `${size} X ${size}`;
-        createGrid(size);
+        const grid = event.target.value;
+        gridSizeLabel.textContent = `${grid} x ${grid}`;
+        createGrid(grid);
     })
     document.getElementById("reset").addEventListener("click", () => {
+        drawBoard.innerHTML = "";
+        pen = defaultPenColor;
+        bgColor = defaultBgColor;
         isRandomActive = false;
         isEraserActive = false;
-
-        pen = defaultPenColor;
         colorInput.value = defaultPenColor;
         bgInput.value = defaultBgColor;
-        bgColor = defaultBgColor;
-        
         gridSize.value = defaultGridSize;
-        gridSizeLabel.textContent = `${defaultGridSize} X ${defaultGridSize}`;
-        randomButton.classList.remove("active");
-        eraserButton.classList.remove("active");
-
+        gridSizeLabel.textContent = `${defaultGridSize} x ${defaultGridSize}`;
         createGrid(defaultGridSize);
     })
 })
